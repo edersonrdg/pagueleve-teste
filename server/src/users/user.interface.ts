7fb52708a2cf61e4657f037797b0ba4cc23619fc
Ederson S <edersonrodrigo31@gmail.com>
2021-07-26 15:35:50 -0300

    feat: add user response interface

---
+++ server/src/users/user.interface.ts
@@ -0,0 +1,6 @@
+export interface UserResponse {
+  token: string;
+  id: string;
+  name: string;
+  email: string;
+}
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AddUserDTO } from './dtos/addUserDTO';
+import { UserResponse } from './user.interface';
 
 @Controller()
 export class UsersController {
   @Post('/signup')
-  async signUp(data: AddUserDTO): Promise<void> {}
+  async signUp(@Body() data: AddUserDTO): Promise<UserResponse> {
+    return Object.assign({ 
+      token: '123', 
+      id: '123', 
+      name: data.name, 
+      email: data.email
+    })
+  }
 }
