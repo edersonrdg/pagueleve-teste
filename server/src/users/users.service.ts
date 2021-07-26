76b25d87c25de3f7cb5f1372ea62eef8c50b25db
Ederson S <edersonrodrigo31@gmail.com>
2021-07-26 15:51:24 -0300

    feat: add criptography to UserService addUser

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { AddUserDTO } from './dtos/addUserDTO';
 import { User } from './user.entity';
 import { UserResponse } from './user.interface';
@@ -12,8 +13,13 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async addUser(data: AddUserDTO): Promise<UserResponse> {
-    const user = this.usersRepository.create(data)
+  async addUser({ email, name, password }: AddUserDTO): Promise<UserResponse> {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = this.usersRepository.create({
+      email,
+      name,
+      password: passwordHash
+    })
     await this.usersRepository.save(user)
     delete user.password
     return Object.assign({ token: '123'}, user)
