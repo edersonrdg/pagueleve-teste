7e74eb107b4426d585dc49f0b5be572e17050ac9
Ederson S <edersonrodrigo31@gmail.com>
2021-07-26 16:16:20 -0300

    feat: add auth module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { User } from './users/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,7 +21,7 @@ import { User } from './users/user.entity';
       database: process.env.DATABASE_NAME,
     entities: [User],
     synchronize: true,
-  }),UsersModule],
+  }),UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
