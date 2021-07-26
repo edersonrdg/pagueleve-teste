f9301c736c351cde8ccd9bc7d9db7bfef2940028
Ederson S <edersonrodrigo31@gmail.com>
2021-07-26 15:43:59 -0300

    feat: add user

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { AddUserDTO } from './dtos/addUserDTO';
+import { User } from './user.entity';
 import { UserResponse } from './user.interface';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   async addUser(data: AddUserDTO): Promise<UserResponse> {
-    return Object.assign({ 
-      token: '123', 
-      id: '123', 
-      name: data.name, 
-      email: data.email
-    })
+    const user = this.usersRepository.create(data)
+    await this.usersRepository.save(user)
+    delete user.password
+    return Object.assign({ token: '123'}, user)
   }
 }
