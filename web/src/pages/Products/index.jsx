f71d0a1c605eff2dfa8abe2b79d493092766517e
Ederson S <edersonrodrigo31@gmail.com>
2021-07-26 22:04:41 -0300

    feat: add product table on page

--- web/src/pages/Products/index.jsx
+++ web/src/pages/Products/index.jsx
@@ -5,7 +5,51 @@ function Products() {
   return (
     <>
       <Sidebar />
-      <h1>Produtos</h1>
+      <div className="main-content">
+      <div id="header">
+        <h1>Produtos</h1>
+        <button type="button" id="btn-primary">Novo produto</button>
+      </div>
+      <div id="product-content">
+        <table id="product-table">
+          <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Data de criação</th>
+            <th>Ultima edição</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>Banana</td>
+            <td>1 kg</td>
+            <td>50 reais</td>
+            <td>40</td>
+            <td>399</td>
+            <td>399</td>
+            <td>
+              <button type="button">Excluir</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Banana</td>
+            <td>1 kg</td>
+            <td>50 reais</td>
+            <td>40</td>
+            <td>399</td>
+            <td>399</td>
+            <td>
+              <button type="button">Excluir</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    )
     </>
   );
 }
