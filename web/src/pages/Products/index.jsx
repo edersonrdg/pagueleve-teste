8ef66874a661cd535e69aa57bdfe53eb7b97800a
Ederson S <edersonrodrigo31@gmail.com>
2021-07-26 22:33:27 -0300

    feat: add create product model

--- web/src/pages/Products/index.jsx
+++ web/src/pages/Products/index.jsx
@@ -1,15 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../components/Sidebar';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import './styles.css'
 
+const useStyles = makeStyles((theme) => ({
+  palette: {
+    primary: {
+      main: '#2C6271'
+    },
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: '6px',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    textAlign: 'center'
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing(2)
+  },
+  input: {
+    marginBottom: theme.spacing(1.4)
+  },
+  button: {
+    backgroundColor: '#2C6271',
+    color: '#FFF',
+    fontWeight: 'bold',
+    padding: theme.spacing(1, 2),
+    border: 'none',
+    borderRadius: '6px',
+    cursor: 'pointer',
+    transitionDuration: '0.3s',
+    '&:hover': {
+      backgroundColor: '#3d8ca1',
+    }
+  }
+}));
+
 function Products() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Sidebar />
       <div className="main-content">
       <div id="header">
         <h1>Produtos</h1>
-        <button type="button" id="btn-primary">Novo produto</button>
+        <button type="button" id="btn-primary" onClick={handleOpen}>Novo produto</button>
+        <Modal
+        aria-labelledby="transition-modal-title"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Novo produto</h2>
+            <form className={classes.form}>
+              <input type="text" className={classes.input} name="name" id="name-product" placeholder="Nome"/>
+              <input type="text" className={classes.input} name="description" id="description" placeholder="Descrição"/>
+              <button type="button" className={classes.button}>Cadastrar</button>
+            </form>
+          </div>
+        </Fade>
+      </Modal>
       </div>
       <div id="product-content">
         <table id="product-table">
