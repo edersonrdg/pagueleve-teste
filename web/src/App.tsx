70d7db088d48072ee932419d76328046930a4d81
Ederson S <edersonrodrigo31@gmail.com>
2021-07-26 21:52:45 -0300

    feat: ensure protucts page exists

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Products from './pages/Products';
 
 function App() {
-  return <h2>Hello world</h2>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/produtos" component={ Products }/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
